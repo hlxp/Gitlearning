54830547e7019570fb4822221e55ee3120b9a85c
hlxp <130364622+hlxp@users.noreply.github.com>
2024-06-23 15:46:51 +0800

    Update fishery_service.py
    修改B2分支代码内容

--- fishery_service.py
+++ fishery_service.py
@@ -5,10 +5,4 @@ from utils.JsonUtils import get_class_list
 
 
 # 获取渔场列表
-def get_fishery_list():
-    sql = "select * from fishery"
-    sqlManager = SQLManager()
-    data = sqlManager.get_list(sql)
-    fishery_list = get_class_list(data, Fishery)
-    sqlManager.close()
-    return deal_data(fishery_list)
+#修改
